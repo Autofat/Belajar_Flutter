c318ce84a5b6a1d1090eefab34682b9d8fdcc556
Galen Borneo Wibowo <borneo.wibowo@gmail.com>
2025-02-03 10:55:28 +0700

    Belajar Navigation and Routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:belajar_flutter/home.dart';
+import 'package:belajar_flutter/pendidikanScreen.dart';
+import 'package:belajar_flutter/screenSecond.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -17,7 +18,47 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueGrey),
         useMaterial3: true,
       ),
-      home: Home(),
+      home: MyHomePage(),
+      initialRoute: '/',
+      routes: {
+        '/screen-second': (context) => ScreenSecondPage(),
+        '/screen-pendidikan': (context) => ScreenPendidikan(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, this.title = 'Flutter Demo Home Page'});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Introduction Page"),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Text("Pengenalan Diri"),
+            ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => ScreenSecondPage()));
+                },
+                child: Text("Next Page"))
+          ],
+        ),
+      ),
     );
   }
 }
