2c3551abaf64d80356fc0b5d0c29bf15bbe47876
Galen Borneo Wibowo <borneo.wibowo@gmail.com>
2025-02-06 17:24:42 +0700

    Feat(CRUD): Menambahkan CRUD dan koneksi ke firebase, jujur lumayan ribet

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,11 @@
-import 'package:belajar_flutter/screenDarkMode.dart';
-import 'package:belajar_flutter/screenSekolah.dart';
-import 'package:belajar_flutter/secondPage.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+
   runApp(const MyApp());
 }
 
@@ -14,58 +15,165 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
+    return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      home: MyHomePage(),
+      home: HomePage(),
       initialRoute: '/',
-      routes: {
-        '/second-screen': (context) => Secondpage(),
-      },
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, this.title = 'Flutter Demo Home Page'});
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<HomePage> createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _HomePageState extends State<HomePage> {
+  final TextEditingController _namaController = TextEditingController();
+  final TextEditingController _statusController = TextEditingController();
+
+  final CollectionReference _absensiSiswa = FirebaseFirestore.instance
+      .collection(
+          'absensi'); //Collection diambil dari nama collection dari firebase
+
+  //Create or Update (Melakukan cek action base dari data yang dikasih)
+  Future<void> _createOrUpdate(DocumentSnapshot? documentSnapshot) async {
+    String action = 'create';
+    if (documentSnapshot != null) {
+      action = 'update';
+      _namaController.text = documentSnapshot['nama'];
+      _statusController.text = documentSnapshot['status'];
+    }
+
+    await showModalBottomSheet(
+      isScrollControlled: true,
+      context: context,
+      builder: (BuildContext context) {
+        return Padding(
+          padding: EdgeInsets.only(
+            top: 20,
+            left: 20,
+            right: 20,
+          ),
+          child: Column(
+            children: [
+              TextField(
+                controller: _namaController,
+                decoration: InputDecoration(
+                  labelText: 'Nama Mahasiswa',
+                ),
+              ),
+              TextField(
+                controller: _statusController,
+                decoration: InputDecoration(
+                  labelText: 'Status Mahasiswa',
+                ),
+              ),
+              SizedBox(
+                height: 5,
+              ),
+              ElevatedButton(
+                onPressed: () async {
+                  final String? nama = _namaController.text;
+                  final String? status = _statusController.text;
+
+                  if (nama != null && status != null) {
+                    if (action == 'create') {
+                      await _absensiSiswa.add({
+                        "nama": nama,
+                        "status": status,
+                      });
+                    }
+                    if (action == 'update') {
+                      await _absensiSiswa.doc(documentSnapshot!.id).update({
+                        "nama": nama,
+                        "status": status,
+                      });
+                    }
+
+                    _namaController.text = '';
+                    _statusController.text = '';
+
+                    Navigator.of(context).pop();
+                  }
+                },
+                child: Text(action == 'create' ? 'Create' : 'Update'),
+              )
+            ],
+          ),
+        );
+      },
+    );
+  }
+
+  // Delete
+  Future<void> _deleteAbsensi(String absensiID) async {
+    await _absensiSiswa.doc(absensiID).delete();
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text("Data berhasil dihapus"),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: Text("Introduction Page"),
-        ),
-        body: Column(children: [
-          Center(
-              child: Column(
-            children: [
-              ElevatedButton(
-                  onPressed: () {
-                    Get.to(Secondpage());
-                  },
-                  child: Icon(Icons.navigate_next_rounded)),
-              ElevatedButton(
-                  onPressed: () {
-                    Get.to(DarkModePage());
-                  },
-                  child: Icon(Icons.dark_mode_rounded)),
-              ElevatedButton(
-                  onPressed: () {
-                    Get.to(Screensekolah());
-                  },
-                  child: Icon(Icons.person)),
-            ],
-          ))
-        ]));
+      appBar: AppBar(
+        title: Text('Absensi Mahasiswa - Firebase'),
+      ),
+      body: StreamBuilder(
+          stream: _absensiSiswa.snapshots(),
+          builder: (context, AsyncSnapshot<QuerySnapshot> streamSnapshot) {
+            if (streamSnapshot.hasData) {
+              return ListView.builder(
+                itemCount: streamSnapshot.data!.docs.length,
+                itemBuilder: (context, index) {
+                  final DocumentSnapshot documentSnapshot =
+                      streamSnapshot.data!.docs[index];
+
+                  return Card(
+                    margin: EdgeInsets.all(10),
+                    child: ListTile(
+                      title: Text(documentSnapshot['nama']),
+                      subtitle: Text(documentSnapshot['status']),
+                      trailing: SizedBox(
+                        width: 100,
+                        child: Row(
+                          children: [
+                            IconButton(
+                                onPressed: () {
+                                  _createOrUpdate(documentSnapshot);
+                                },
+                                icon: Icon(Icons.edit)),
+                            IconButton(
+                                onPressed: () {
+                                  _deleteAbsensi(documentSnapshot.id);
+                                },
+                                icon: Icon(Icons.delete)),
+                          ],
+                        ),
+                      ),
+                    ),
+                  );
+                },
+              );
+            }
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _createOrUpdate(null),
+        child: Icon(Icons.add),
+      ),
+    );
   }
 }
